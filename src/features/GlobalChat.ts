798beff5ba968319bbcf8e36bf38051e80df2fe3
Patrick Bowen <phunanon@users.noreply.github.com>
2024-06-13 19:44:25 +0100

    Ability to edit GlobalChat messages

--- src/features/GlobalChat.ts
+++ src/features/GlobalChat.ts
@@ -2,6 +2,10 @@ import { ApplicationCommandOptionType, ChannelType } from 'discord.js';
 import { Feature } from '.';
 import { prisma } from '../infrastructure';
 
+const bufferLen = 10_000;
+const m2m: { channelId: string; messageId: string }[][] = [];
+let m2mIndex = 0;
+
 export const GlobalChat: Feature = {
   async Init(commands) {
     await commands.create({
@@ -31,14 +35,20 @@ export const GlobalChat: Feature = {
     const member = await guild.members.fetch(message.author.id);
     const nickname =
       member.displayName ?? member.nickname ?? message.author.tag;
-    const asterisk = isEdit ? '* ' : '';
     const truncated =
       message.content.length > 1500
         ? `${message.content.slice(0, 1500)}...`
         : message.content;
-    const content = `**${nickname}**: ${asterisk}${truncated}`;
+    const content = `**${nickname}**: ${truncated}`;
     const files = message.attachments.map(a => a.url);
+    const payload = { content, files, allowedMentions: { parse: [] } };
 
+    const mids: (typeof m2m)[0] = [
+      { channelId: message.channel.id, messageId: message.id },
+    ];
+    const associations = m2m.find(m =>
+      m.some(({ messageId }) => messageId === message.id),
+    );
     for (const { channelSf } of guilds) {
       const channel = await (async () => {
         try {
@@ -51,7 +61,30 @@ export const GlobalChat: Feature = {
         await prisma.globalChat.delete({ where: { channelSf } });
         continue;
       }
-      await channel.send({ content, files, allowedMentions: { parse: [] } });
+
+      if (isEdit) {
+        if (associations) {
+          for (const { messageId } of associations) {
+            if (messageId === message.id) continue;
+            try {
+              await channel.messages
+                .fetch(messageId)
+                .then(msg => msg.edit(payload));
+            } catch (e) {}
+          }
+          continue;
+        }
+      } else {
+        const msg = await channel.send(payload);
+        mids.push({ channelId: channel.id, messageId: msg.id });
+      }
+    }
+
+    if (m2m.length === bufferLen) {
+      m2m[m2mIndex] = mids;
+      m2mIndex = (m2mIndex + 1) % bufferLen;
+    } else {
+      m2m.push(mids);
     }
   },
   Interaction: {
